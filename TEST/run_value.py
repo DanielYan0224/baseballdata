77c7b2ca1be7038c1bd48365e48c0d58924440c1
DanielYan0224 <yourmail@gmail.com>
2024-03-10 00:49:34 +0800

    0310

--- TEST/run_value.py
+++ TEST/run_value.py
@@ -4,14 +4,15 @@ import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
 
+df = pd.read_csv(r"C:\Users\閻天立\Desktop\pybaseball\data2019.csv")
 def run_expectancy(df, level='plate appearence'):
 
-    # 排序
-    df.cort_values(by = by=['game_pk', 'at_bat_number', 'pitch_number'], inplace=True)
+# 排序
+df.sort_values(by = ['game_pk', 'at_bat_number', 'pitch_number'], inplace=True)
 
-    # last pitch
-    df['last_pitch_at_bat'] = df.groupby(['game_pk', 'at_bat_number', 'inning_topbot'])\
-    ['pitch_number'].transform(lambda x: (x == x.max()).astype(int))
+# last pitch
+df['last_pitch_at_bat'] = df.groupby(['game_pk', 'at_bat_number', 'inning_topbot'])\
+['pitch_number'].transform(lambda x: (x == x.max()).astype(int))
 
-    print
+
 #%%
