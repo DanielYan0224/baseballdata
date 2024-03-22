131f9b6900ea97837362981a1eec4d30a56c616f
DanielYan0224 <tianliyan@gmail.com>
2024-03-22 15:25:31 +0800

    Calculate DE JE CE

--- TEST/entropy.py
+++ TEST/entropy.py
@@ -6,31 +6,62 @@ import plotly.express as px
 from scipy.stats import entropy
 
 # Define Discrete Entropy
-def DE(data, bins_edge):
-    hist, _ = np.histogram(data, bins=bins_edge)
+def DE(data_x, bins_x):
+    hist, _ = np.histogram(data_x, bins=bins_x)
     prob = hist / np.sum(hist)
     prob = prob[prob > 0]
 
     return entropy(prob, base=2)
 
+# H(X,Y)
+def JE(data_x, data_y, bins_x, bins_y):
+    hist, _, _ = np.histogram2d(data_x, data_y, bins=[bins_x, bins_y])
+    prob = hist / np.sum(hist)
+    prob = prob[prob > 0]
+
+    return entropy(prob.flatten(), base=2)
+
+#H(Y|X)=H(X,Y)-H(X)
+def CE(data_x, data_y, bins_x, bins_y):
+    conditional_entropy = \
+    JE(data_x, data_y, bins_x, bins_y) - DE(data_x, bins_x)
+
+    return conditional_entropy
 ###########################################
 # Import Data
 file_path = r"C:\Users\user\Desktop\baseballdata\data2022.csv" 
 df = pd.read_csv(file_path)
-###########################################
-# Cut the Data
-bins_edge = [0, 70, 90, 100, 110, 120]
-df['launch_speed'] = df['launch_speed'].fillna(0)
-df['launch_speed_histro'] = pd.cut(df['launch_speed'], bins=bins_edge, right=False)
 
-# 把切割的區間轉成字串顯示在histrogram
-df['launch_speed_histro'] = df['launch_speed_histro'].astype(str)
+x = 'launch_speed'
+y = 'launch_angle'
+data_x = df[x]
+data_y = df[y]
 
-print(DE(df['launch_speed'], bins_edge))
-
-# Plot histrogram
-fig = px.histogram(df, x='launch_speed_histro', \
-histnorm='probability density',\
+fig = px.histogram(df[x], x=x, histnorm='probability density',\
 title='Histogram of Launch Speed Ranges', text_auto=True)
 fig.write_html('histrogram.html', auto_open=True)
-#%%
+print(DE(data_x, bins_x))
+print(DE(df['launch_angle'], bins_x))
+
+###########################################
+# Cut the Data
+bins_x = [0, 70, 90, 100, 110, 120]
+bins_y = [-80, -20, -10, 0, 40, 90]
+
+###########################################
+df['launch_speed'] = df['launch_speed'].fillna(0)
+df[f'{x}_histro'] = pd.cut(df[x], \
+bins=bins_x, right=False)
+
+print(df[f'{x}_histro'], df[x])
+
+# 把切割的區間轉成字串顯示在histrogram
+df[f'{x}_histro'] = df[f'{x}_histro'].astype(str)
+
+# Plot histrogram
+fig = px.histogram(df, x=f'{x}_histro', \
+histnorm='probability density',\
+title='Histogram of Launch Speed Ranges', text_auto=True)
+fig.write_html('hisstrogram.html', auto_open=True)
+###########################################
+#%% 
