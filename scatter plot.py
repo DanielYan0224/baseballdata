990c5b85f5019fa098c59f8e2e27c7e76500ad09
DanielYan0224 <tianliyan@gmail.com>
2024-03-22 16:54:52 +0800

    add grid

--- scatter plot.py
+++ scatter plot.py
@@ -20,28 +20,37 @@ data_2023.insert(1, 'year', '2023')
 cole_2223 = merged_data = pd.concat([data_2022, data_2023], axis=0)
 cole_2223.to_csv(r"C:\Users\user\Desktop\baseballdata\cole_2223.csv")
 
-year = '2023'
-cole_2223 = cole_2223[cole_2223['year'] == year]
+# year = '2023'
+# cole_2223 = cole_2223[cole_2223['year'] == year]
 
 x_label = 'launch_angle'
 y_label = 'launch_speed'
-x = cole_2223[x_label]
-y = cole_2223[y_label]
+
 
 # 定義條件以更改顏色
-condition_gray = (cole_2223['events'] == 'home_run')
-condition_black = ~condition_gray  # 反轉條件
+condition = (cole_2223['events'] == 'field_out')
+condition_reverse = ~condition  # 反轉條件
 
 # 將符合特定條件的數據點移至數據框頂部
-data_sorted = pd.concat([cole_2223[condition_gray], cole_2223[~condition_gray]])
+data_sorted = pd.concat([cole_2223[condition], cole_2223[~condition]])
 
-fig = px.scatter(data_sorted, x=x, y=y, color=condition_gray, color_discrete_map={True: 'red', False: 'gray'}, 
-                symbol=condition_gray, symbol_map={True: 'diamond', False: 'circle'})  
+fig = px.scatter(data_sorted, x=x_label, y=y_label, color=condition, color_discrete_map={True: 'red', False: 'gray'}, 
+symbol=condition, marginal_x="histogram", marginal_y="histogram",
+symbol_map={True: 'diamond', False: 'circle'})
 
-fig.update_xaxes(title_text = x_label)
-fig.update_yaxes(title_text = y_label)
-fig.update_layout(title= year, title_x=0.5, title_y=0.95)
+# fig.update_xaxes(title_text = x_label)
+# fig.update_yaxes(title_text = y_label)
+# fig.update_layout(title= year, title_x=0.5, title_y=0.95)
 
+fig.add_shape(type="line", 
+            x0=30, y0=0, x1=30, y1=1, 
+            xref="x", yref="paper",  
+            line=dict(color="RoyalBlue", width=1))
+
+fig.add_shape(type="line", 
+            x0=0, y0=80, x1=1, y1=80, 
+            xref="paper", yref="y",  
+            line=dict(color="RoyalBlue", width=1))
 # 將散點圖儲存為 HTML
 fig.write_html('scatter_plot.html', auto_open=True)
 
