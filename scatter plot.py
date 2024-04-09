25c8cdc0946e058ebf80556be58f9d9164590885
DanielYan0224 <tianliyan@gmail.com>
2024-04-10 00:01:04 +0800

    新增區域分割

--- scatter plot.py
+++ scatter plot.py
@@ -34,45 +34,38 @@ bins_x = [df[x_label].min(), \
         0, 40, df[x_label].max()]
 bins_y = [df[y_label].min(), 60, 80, df[y_label].min()]
 
-# 定義每個安打的價值
+# 只留下安打與出局
 def new_event(events):
-    mapping = {'single': 1, 'double': 2, 'triple': 3, 'home_run': 4, 'field_out': 0}
-    return mapping.get(events, 0)  # 默认返回0
-
-# 定義分割的區間
-def assign_zone(df):
-    if df['launch_angle'] < 0:
-        if df['launch_speed'] < 60:
-            return 'A'  # Zone A
-        elif 60 <= df['launch_speed'] < 80:
-            return 'B'  # Zone B
-        else:
-            return 'C'  # Zone C
-    elif 0 <= df['launch_angle'] < 40:
-        if 60 <= df['launch_speed'] < 80:
-            return 'D'  # Zone D
-    elif df['launch_angle'] >= 40:
-        if 60 <= df['launch_speed'] < 80:
-            return 'E'  # Zone E
-    return 'Other'  # 其他区域
-
-df['Zone'] = df.apply(assign_zone, axis=1)
-df['event_numeric'] = df['events'].apply(new_event).astype(str)  # 确保这里的列名与您的DataFrame中的列名匹配
-
-color_discrete_map = {
-    '1': 'blue',   
-    '2': 'orange',
-    '3': 'green',
-    '4': 'red',
-    '0': 'purple'
-}
+    mapping = {'single': "single", 'double': "double", 
+            'triple': "triple", 'home_run': "home_run", 
+            'field_out': "field_out"}
+    return mapping.get(events, "others")  # 其他的都是0
+df['new_event'] = df['events'].apply(new_event).astype(str)  # 确保这里的列名与您的DataFrame中的列名匹配
 
 
+# 新增標線
 fig = px.scatter(df, x=x_label, y=y_label, \
-        color='event_numeric', \
+        color='new_event', \
         color_discrete_map=color_discrete_map,
         marginal_x="histogram", marginal_y="histogram",
 )
+
+symbol_map = {
+    'single': 'circle',
+    'double': 'square',
+    'triple': 'diamond',
+    'home_run': 'star',
+    'field_out': 'x',
+    'others': 'triangle-up'
+}
+color_discrete_map = {
+    'single': 'blue',
+    'double': 'orange',
+    'triple': 'green',
+    'home_run': 'red',
+    'field_out': 'purple',
+    'others': 'grey'
+}
 
 fig.add_shape(type="line", 
         x0=df['launch_angle'].min(), y0=bins_y[1],
@@ -96,15 +89,28 @@ fig.add_shape(type="line",
         line=dict(color="black", width=1.5))
 
 fig.write_html('allpitcher.html', auto_open=True)
-#%%
-widgets.interactive(update_scatter_plot, pitcher_id=pitcher_dropdown)
+
+# Classify zone by LA and LS
+df.loc[(df[x_label] < 0) & (df[y_label] < 60), 'classify_zone'] = "A"
+df.loc[(df[x_label] < 0) & (df[y_label] >= 60), 'classify_zone'] = "B"
+df.loc[(0 <= df[x_label]) & (df[x_label] < 40) &
+    (bins_y[0] <= df[y_label]) & (df[y_label] <= 80)
+    , "classify_zone"] = "C"
+df.loc[(0 <= df[x_label]) & (df[x_label] < 40) \
+    & (80 < df[y_label] ) & (df[y_label] <= bins_y[3])
+    , 'classify_zone'] = 'D'
+df.loc[(40 <= df[x_label]), 'classify_zone'] = "E"
+
+
+df_E = df[df["classify_zone"] == "E"]
+print(df_E["new_event"].value_counts(normalize= True))
 #%%
 import plotly.graph_objects as go
 import pandas as pd
 import ipywidgets as widgets
 from IPython.display import display
 
-# 加载数据和预处理...
+
 file_path = r"C:\Users\user\Desktop\baseballdata\2022sppitchingdata.csv"
 df = pd.read_csv(file_path)
 df = df[df['description'] == 'hit_into_play']
@@ -128,13 +134,13 @@ def new_event(events):
 df['new_events'] = df['events'].apply(new_event)
 
 def update_scatter_plot(pitcher_id):
-    # 标记是否为选定的投手
+    # 標記選定的選手
     df['highlight'] = df['pitcher'] == pitcher_id
     
-    # 使用 plotly.graph_objects 创建散点图，以便更细致地控制颜色
+    # 
     fig = go.Figure()
     
-    # 添加不是选定投手的数据点，颜色设置为灰色
+    # 非選定選手的資料
     fig.add_trace(go.Scatter(x=df[~df['highlight']][x_label], y=df[~df['highlight']][y_label],
                         mode='markers', marker_color='lightgrey', name='Others'))
     symbol_map = {
@@ -153,26 +159,21 @@ def update_scatter_plot(pitcher_id):
     'field_out': 'purple',
     'others': 'grey'
 }
-    # 添加选定投手的数据点，使用 new_events 来设置颜色
+
     for event, group_df in df[df['highlight']].groupby('new_events'):
         fig.add_trace(go.Scatter(x=group_df[x_label], y=group_df[y_label], mode='markers',
         marker_symbol=symbol_map[event], 
         name=event, marker_color=color_discrete_map[event]))
-    
-    # 显示图表
     fig.show()
 
-# 创建下拉菜单
 pitcher_dropdown = widgets.Dropdown(
     options=[('Select a pitcher', None)] + [(str(id), id) for id in df['pitcher'].unique()],
     description='Pitcher ID:',
-    value=None  # 默认没有选中任何投手
+    value=None 
 )
 
-# 创建一个用于更新散点图的互动控件
 interactive_plot = widgets.interactive(update_scatter_plot, pitcher_id=pitcher_dropdown)
 
-# 显示下拉菜单
 display(interactive_plot)
 
 #%%
